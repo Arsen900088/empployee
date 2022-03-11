11752e67fc2631f0e47f2dfdd76db715917e9225
Arsen <javascript000001@gmail.com>
2022-03-11 11:12:35 +0400

    Add ! symbol in footer component

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -4,7 +4,7 @@ const Footer = () => {
     return (
     <footer>
         <div id={styles.text_footera}>
-            <h6>Добавьте нового сотрудника</h6>
+            <h6>Добавьте нового сотрудника!</h6>
         </div>
         <form id={styles.input_footera}>
             <input type="text" id={styles.kak_zovut} placeholder="Как его зовут?" minLength="3" />
